d5a956fa1ae89750faa8f00af7c8f3680248eac9
Luke Shay <lshay@sourceallies.com>
2020-10-18 14:45:11 -0500

    try some tings

--- lib/client/hooks.ts
+++ lib/client/hooks.ts
@@ -1,9 +1,7 @@
 import { useQuery } from '@apollo/client';
-import { useRouter } from 'next/router';
 import React from 'react';
-import { IUser, Optional, UserRole } from '../types';
+import { IUser, Optional } from '../types';
 import { ViewerQuery } from './graphql/server/queries';
-import { Paths } from './paths';
 
 export function useViewer() {
   const [viewer, setViewer] = React.useState<Optional<IUser>>();
